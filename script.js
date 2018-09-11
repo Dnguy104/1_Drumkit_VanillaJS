01d18afa9642d7a2ce823ecf513a18d94787be15
Dnguy104 <dnguy104@ucr.edu>
2018-09-10 21:47:47 -0700

    first commit: adding drum kit project

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function keyStartFunc(e) {
+	// console.log(e);
+	var audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+	var key = document.querySelector(`.keys[data-key = "${e.keyCode}"]`);
+	if (!audio) return;
+	
+	audio.currentTime = 0;
+	audio.play()
+	key.classList.add('playing');
+	
+}
+
+function keyEndFunc(e) {
+	if(e.propertyName !== 'transform') return;
+	
+	this.classList.remove('playing');
+}
+var keys = document.querySelectorAll('.keys');
+
+
+keys.forEach(key => key.addEventListener('transitionend', keyEndFunc));
+window.addEventListener('keydown', keyStartFunc);
